99417d8bb1d7651e4a7e35a534504e620a8d0482
IndigoQuadratic <107479389+IndigoQuadratic@users.noreply.github.com>
2023-07-25 10:59:57 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -67,7 +67,7 @@ select.addEventListener("click", function () { elementToggleFunc(this); });
 for (let i = 0; i < selectItems.length; i++) {
   selectItems[i].addEventListener("click", function () {
 
-    let selectedValue = this.innerText;
+    let selectedValue = this.innerText.toLowerCase();
     selectValue.innerText = this.innerText;
     elementToggleFunc(select);
     filterFunc(selectedValue);
